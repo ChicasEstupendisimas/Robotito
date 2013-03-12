ab27a6c103d7707e3378e207df171fea368eaada
marpl136 <marpl136@student.liu.se>
2013-03-12 11:47:48 +0100

    Clase principal añadida!

---
+++ src/BattleTech.java
@@ -0,0 +1,22 @@
+/**
+ * Ana Ferreira Blanco y Mar’a Jesœs Platero S‡nchez
+ * Robotito de la muerte versi—n: Vamos a aprobar
+ * Clase principal desde la que se hace de t—.
+ * 
+ */
+
+
+public class BattleTech {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
--- src/prueba.java
+++
@@ -1,7 +0,0 @@
-
-public class prueba {
-
-	//LA PUTA MADRE QUE PARIÓ A ECLIPSE, LOS REPOSITORIOS, ICO Y DEMÁS HIJOS DE LA GRAN CHINGADA
-	
-	//Mae miaaa que hartura
-}
