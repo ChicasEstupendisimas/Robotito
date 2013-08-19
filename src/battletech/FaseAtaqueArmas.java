5af2ea7093bf69695cd83e1e377060d54d60e97a
CookieBrownie <tu.nenita@gmail.com>
2013-08-19 11:43:28 +0200

    ana subiendo cosicas del ataque con armas: declaraciones

--- src/battletech/FaseAtaqueArmas.java
+++ src/battletech/FaseAtaqueArmas.java
@@ -5,8 +5,20 @@ import java.util.ArrayList;
 public class FaseAtaqueArmas {
 
 public  ArrayList<String> IniciaFaseAtaqueArmas(){
-	return null;
-		
+	
+	Configuracion configuracion;
+    Tablero mapa;
+    ArrayList<Mech> robots;
+    Mech miMech;
+    ArrayList <Armas> armas;
+    ArrayList <int[]> armasDisparadas ;
+    int colObj=-1, filObj=-1;
+
+    ArrayList <String> archivo[];	
+    
+    
+    
+    return null;
 		
 	}
 }
